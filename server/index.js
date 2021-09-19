ea5291be81a2dc5107e218f3261f129c909f87f5
minjunsz <minjunsz@postech.ac.kr>
2021-09-19 01:53:09 +0000

    check database connection

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1234',
+  database : 'my_db',
+});
+
+connection.connect();
+
+console.log("connected");
+
+
+// connection.query("CREATE DATABASE IF NOT EXISTS my_db;");
+
+connection.end();
