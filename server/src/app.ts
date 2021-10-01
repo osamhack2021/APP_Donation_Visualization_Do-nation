46d2171f5ceebce4149962c1947a325f5c4edbdd
minjunsz <minjunsz@postech.ac.kr>
2021-10-01 10:04:30 +0000

    add swaggerUI

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,18 @@
-import express from "express"
+import express, { Request as ExRequest, Response as ExResponse } from "express"
 import { RegisterRoutes } from "./routes/routes"
+import swaggerUI from "swagger-ui-express"
 
 const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use("/docs", swaggerUI.serve, async (_req: ExRequest, _res: ExResponse) => {
+    return _res.send(
+        swaggerUI.generateHTML(await import("../build/swagger.json"))
+    );
+})
 
 RegisterRoutes(app);
 
