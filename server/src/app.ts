f984fd550661d8fdea48da7134e300e9f01c70d9
minjunsz <minjunsz@postech.ac.kr>
2021-10-03 14:32:52 +0000

    clean up project structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,39 +5,35 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Target } from "./entity/target";
 import { Donation } from "./entity/donation";
+import { PORT, __prod__ } from "./constants";
 
 (async () => {
-  const port = process.env.PORT || 8000;
-  const app = express();
-
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-
-  createConnection({
+  await createConnection({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
     password: "1234",
     database: "do_nation",
-    synchronize: true,
-    logging: true,
+    synchronize: !__prod__,
+    logging: !__prod__,
     entities: [Target, Donation],
-  });
+  }).catch((error) => console.error(`connection error: ${error}`));
 
+  const app = express();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
   app.use(
     "/docs",
     swaggerUI.serve,
     async (_req: ExRequest, _res: ExResponse) => {
-      return _res.send(
-        swaggerUI.generateHTML(await import("../build/swagger.json"))
-      );
+      return _res.send(swaggerUI.generateHTML(await import("../swagger.json")));
     }
   );
 
   RegisterRoutes(app);
 
-  app.listen(port, () =>
-    console.log(`Express server is running on http://localhost:${port}`)
+  app.listen(PORT, () =>
+    console.log(`Express server is running on http://localhost:${PORT}`)
   );
 })();
