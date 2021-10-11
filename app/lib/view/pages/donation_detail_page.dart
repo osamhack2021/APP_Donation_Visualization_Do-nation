6376694cd26053f03e35083cce03d8192556c89a
minjunsz <minjunsz@postech.ac.kr>
2021-10-11 14:43:39 +0000

    make basic layout

--- app/lib/view/pages/donation_detail_page.dart
+++ app/lib/view/pages/donation_detail_page.dart
@@ -16,20 +16,107 @@ class DonationDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Donation Detail'),
       ),
-      body: ElevatedButton(
-        onPressed: () async {
-          dynamic result = await Get.defaultDialog(
-            title: "기부 참가하기",
-            content: const DonationDialog(),
-          );
-          if (result["done"]) {
-            final creationDTO = (result["dto"] as DonationCreationDTO);
-            creationDTO.targetId = target.id!;
-            donationController.save(creationDTO);
-          }
-        },
-        child: Text('${target.name}에게 기부하기'),
+      body: Container(
+        color: Colors.amber,
+        child: SizedBox(
+          height: double.infinity,
+          child: Column(
+            children: [
+              Container(
+                height: 100.0,
+                color: Colors.teal,
+              ), // some top content
+
+              Expanded(
+                child: Center(
+                  child: ListView(
+                    children: [], //your list view content here
+                  ),
+                ),
+              ),
+
+              Container(
+                color: Colors.lightGreen,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      _MakeDonationButton(
+                          target: target,
+                          donationController: donationController),
+                    ],
+                  ),
+                ),
+              ), // some bottom content
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+class _MakeDonationButton extends StatelessWidget {
+  const _MakeDonationButton({
+    Key? key,
+    required this.target,
+    required this.donationController,
+  }) : super(key: key);
+
+  final Target target;
+  final DonationController donationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        dynamic result = await Get.defaultDialog(
+          title: "기부 참가하기",
+          content: const DonationDialog(),
+        );
+        if (result["done"]) {
+          final creationDTO = (result["dto"] as DonationCreationDTO);
+          creationDTO.targetId = target.id!;
+          donationController.save(creationDTO);
+        }
+      },
+      child: Text('${target.name}에게 기부하기'),
+    );
+  }
+}
+
+
+/*Column(
+          children: [
+            Container(
+              height: 10.0,
+              color: Colors.teal,
+            ), // some top content
+
+            Container(
+              color: Colors.amber,
+              child: Expanded(
+                child: Center(
+                  child: ListView(
+                    children: [], //your list view content here
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              color: Colors.lightGreen,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    _MakeDonationButton(
+                        target: target, donationController: donationController),
+                  ],
+                ),
+              ),
+            ), // some bottom content
+          ],
+        )*/
